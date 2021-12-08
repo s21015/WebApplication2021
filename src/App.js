496e4e615c40d5b83ed8a8933276428075f6dfba
s21015 <s21015@std.it-college.ac.jp>
2021-12-08 02:33:26 +0000

    pokemon app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Header from './Header'
+import Search from './Search'
+import './App.css'
 
-function App() {
+const url = 'https://pokeapi.co/api/v2/pokemon/1'
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [pokemons, setPokemons] = useState([])
+  const [pokeNum, setPokeNum] = useState()
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setPokemons(jsonResponse.name)
+        setPokeNum(jsonResponse.id)
+        setLoading(false)
+      })
+  }, [])
+
+  const search = searchValue => {
+    setLoading(true)
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}/`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setPokemons(jsonResponse.name)
+        setPokeNum(jsonResponse.id)
+        setLoading(false)
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <Search search={search} />
+      <div className="pokemons">
+        {loading ? (
+          <span>loading...</span>
+        ) : (
+          <div>
+            <h2>No.{pokeNum} : {pokemons}</h2>
+            <img
+              width="200"
+              alt="pokemon"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeNum}.png`}
+            />
+            <img
+              width="200"
+              alt="pokemon_shiny"
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokeNum}.png`}
+            />
+          </div>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = props => {
+  return (
+    <header className='App-header'>
+      <img
+        width='300'
+        alt='pokemon logo'
+        src='https://www.pokemon.co.jp/img/logo.png'
+      />
+    </header>
+  )
+}
+
+export default Header
